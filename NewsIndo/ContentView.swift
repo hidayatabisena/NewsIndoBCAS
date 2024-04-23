e450adadc79177e275c9853611f5cd801b3fe4a5
Han Sena <abisena@outlook.com>
2024-04-23 08:47:47 +0700

    Initial Commit

---
+++ NewsIndo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsIndo
+//
+//  Created by Hidayat Abisena on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsIndo/NewsIndoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsIndoApp.swift
+//  NewsIndo
+//
+//  Created by Hidayat Abisena on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsIndoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
